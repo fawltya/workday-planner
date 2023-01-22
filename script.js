b68231ce641ca59bc562e0d675cf2075042c0208
Seamus Barnett <seamus0689@gmail.com>
2023-01-22 10:45:36 +0000

    future/prev coloured rows working + console log form input to array correct

--- script.js
+++ script.js
@@ -9,33 +9,81 @@ function showDate() {
 showDate()
 setInterval(showDate, 1000);
 var now = moment().format('H') // current hour
-console.log(now);
 
 timeblocks = $('.planner-input-row')
+
 timeblocks.each(function() {
     var hour = $(this).attr('data-hour');
-    hour = parseInt(hour, 10);
-    now = parseInt(hour, 10);
+    hour = parseInt(hour, 10)
+    now = parseInt(now, 10)
+
     if (hour < now) {
-        $(this).css('background-color', 'var(--past-color)')
-    } if (hour === now) {
-        $(this).css('background-color', 'var(--present-color)')
-    } else $(this).css('background-color', 'var(--future-color)')
+        $(this).css('background-color', 'red')
+    } else if (hour == now) {
+        $(this).css('background-color', 'green')
+    } else {
+        $(this).css('background-color', 'purple')
+        
+    }
+    return thisHour = hour;
+
 })
 
+var inputs = [];
+var input = $('input')
 
-
-var formInput = $('planner-input-row')
-var plannerSubmitValue = $('#9am-form');
-
-var formHandle = function (event) {
+$('.planner-input-row').on( 'submit', function(event) {
     event.preventDefault();
-    var plannerSubmit = plannerSubmitValue.val();
+    // for (var i = 0; i < 2; i++) {
+    inputs.push('time: ' + thisHour, 'input: ' + input.val());
+    console.log(inputs)
+    $('input').val('')
+    // console.log(input)
+//   }
+} );
 
-    plannerSubmit.appendTo(plannerSubmitValue);
+//   console.log(inputs)
 
-}
-// formHandle();
 
-// formEl.on('submit', handleFormSubmit);
-formInput.on('submit', formHandle);
+
+
+// 3. Save input to local storage
+//      create variable (an array) called inputs that will store all of the input data
+//      Add event listener to all save button
+//          Add event.preventDefault inside the click event listener
+//          Push the input value to inputs array with the format of { time: xx, input: xxx }
+//              Get the input value
+//              Get the hour value
+//              Push to the inputs array if the hour entry not exist yet in the array
+//              Replace the existing entry if the hour entry exist in the array
+//          Save inputs variable to local storage
+//              Stringify the inputs array
+//          Show feedback message to the user (optional)
+//          The feedback need to be dissappeared automatically
+//
+//      Example of inputs array format
+//      var inputs = [{
+//        time: 9,
+//        input: 'Meeting',
+//      },{
+//        time: 10,
+//        input: 'Coffee',
+//      },...];
+
+
+
+
+// var formInput = $('planner-input-row')
+// var plannerSubmitValue = $('#9am-form');
+
+// var formHandle = function (event) {
+//     event.preventDefault();
+//     var plannerSubmit = plannerSubmitValue.val();
+
+//     plannerSubmit.appendTo(plannerSubmitValue);
+
+// }
+// // formHandle();
+
+// // formEl.on('submit', handleFormSubmit);
+// formInput.on('submit', formHandle);
